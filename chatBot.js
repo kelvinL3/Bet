83e60e769a07fff2e0b9cd96e7b1d624fe2ea6df
Kelvin <kelvinliu1234@gmail.com>
2017-04-22 15:41:53 -0400

    adding a new chatbot to gmail and fb

---
+++ chatBot.js
@@ -0,0 +1,10 @@
+const login = require("facebook-chat-api");
+
+// Create simple echo bot
+login({email: "FB_EMAIL", password: "FB_PASSWORD"}, (err, api) => {
+    if(err) return console.error(err);
+
+    api.listen((err, message) => {
+        api.sendMessage(message.body, message.threadID);
+    });
+});
