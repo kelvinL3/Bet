4742bfadbf4e8db30d34beaa1f97546d4f0dd593
Kelvin <kelvinliu1234@gmail.com>
2017-04-22 23:27:03 -0400

    updated

--- chatBot.js
+++ chatBot.js
@@ -1,9 +1,11 @@
 const login = require("facebook-chat-api");
 var config = require('./config.js')
 var flag1 = false;
+var flag1a = false;
 var flag2 = false;
 var flag3 = false;
 var currentP1ID = 0;
+var betString = null;
 var currentP2ID = 0;
 var money = null;
 var canJudge = 0;
@@ -14,6 +16,7 @@ var f1 = false;
 var f2 = false;
 var finalConfirmation = false;
 var currentThread = 0;
+var whichWon = -1;
 
 //this is a callback, the input to the callback is the output of the function 
 var x = function (err, api) { 
@@ -39,7 +42,7 @@ var x = function (err, api) {
     		api.sendMessage("Type \"bet [name]\" to start a bet \n \
     			Type info for group chat info \
     			Type betinfo for current bet info \
-    			Type exit to cancel bet(if you are the better or betee)", message.threadID);
+    			Type exit to cancel bet(can if you are the better or betee)", message.threadID);
     	}
 
     	if (tokens[0].valueOf()==="info".valueOf()){
@@ -52,26 +55,45 @@ var x = function (err, api) {
     		api.sendMessage("Player 1: "+currentP1ID+"Player 2: "+currentP2ID+"for this amount: "+money, message.threadID);
     	}
     	
+    	//-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+		if (tokens[0].valueOf()==="exit".valueOf()&&(message.senderID===currentP1ID||message.senderID===currentP2ID)) { //cancel bet
+		    		flag1 = false;
+					flag1a = false;
+					flag2 = false;
+					flag3 = false;
+					currentP1ID = 0;
+					betString = null;
+					currentP2ID = 0;
+					money = null;
+					canJudge = 0;
+					finJudge = 0;
+					binary =-1;
+					Cstep = false;
+					f1 = false;
+					f2 = false;
+					finalConfirmation = false;
+					currentThread = 0;
+					whichWon = -1;
+		    		api.sendMessage("Reset!", message.threadID);
+		}
     	//BETS START HERE!!!
     	if (tokens[0].valueOf()==="bet".valueOf()) {
-    		api.sendMessage("Start a bet with " + tokens[1] + " for how much? (give a number)", message.threadID);
+    		api.sendMessage("Start a bet with " + tokens[1] + " for how much? (give a number)\
+    			Also enter what the winning condition is.", message.threadID);
     		flag1 = true;
     		currentP1ID = message.senderID;
     		currentP2ID = tokens[1];
     	}
 
-    	if (tokens[0].valueOf()==="exit".valueOf()&&(message.senderID===currentP1ID||message.senderID===currentP2ID)) { //cancel bet
-    		flag1 = false;
-			flag2 = false;
-			flag3 = false;
-			currentP1ID = 0;
-			currentP2ID = 0;
-			money = null;
-			canJudge = 0;
-			binary =-1;
-			currentThread = 0;
-    		api.sendMessage("Done!", message.threadID);
+    	if (tokens[0].valueOf()==="outcome".valueOf()&&(flag1a===true)&&(message.senderID==currentP1ID)) { //add condition player 1 must be the one to enter
+    		api.sendMessage("Start a bet with " + tokens[1] + " for how much? (give a number)", message.threadID);
+    		flag1 = true;
+    		flag1a = false;
+    		
+    		betString = message.substring(message.indexOf(" "));
     	}
+
+    	
     	if (flag1===true&&message.senderID==currentP1ID) { //to prompt for the amount of money
     			if (isNaN(message.body)) {
     				api.sendMessage("Error, not a number! Try again! (quit to start over)", message.threadID);
@@ -100,7 +122,7 @@ var x = function (err, api) {
 			api.sendMessage("The final judge is " + finJudge, message.threadID);
 			Cstep = true;
     	}
-    	if ((Cstep===true)&&(message.senderID===currentP1ID||message.senderID===currentP2ID)) {
+    	if ((Cstep===true)&&(message.senderID===currentP1ID||message.senderID===currentP2ID)) { //to confirm
 	    	if ((tokens[0]==="yes")&&(message.senderID===currentP1ID)) {
 	    		f1="yes";
 	    	}
@@ -108,7 +130,8 @@ var x = function (err, api) {
 	    		f2="yes";
 	    	}
 	    	if (f1==="yes"&&f2==="yes") {
-				api.sendMessage("Everyone has agreed! Bet has started!" + canJudge, message.threadID);
+				api.sendMessage("Everyone has agreed! Bet has started!" + canJudge +"\n \
+					Judge, enter \"win: player\" to confirm the winner.", message.threadID);
 				Cstep=false;
 				f1=false;
 				f2=false;
@@ -124,8 +147,21 @@ var x = function (err, api) {
 	    	}
 	    }
 
+	    if ((finalConfirmation===true)&&(message.senderID===finJudge)&&(tokens[0]==="win:")) { // to determine winner
+	    	getID(api, tokens[1], message.threadID , function(err, id) { 
+	    	var temp = id
+	    	if (temp===currentP1ID) {
+	    		api.sendMessage(tokens[1] + canJudge, message.threadID);
+	    		whichWon=1;
+	    	} else if (temp===currentP2ID) {
+	    		api.sendMessage(tokens[1] + canJudge, message.threadID);
+	    		whichWon=2;
+	    	} else {
+	    		console.log("error!")
+	    	}
+	    	 });
 
-
+	    }
 
 
 
